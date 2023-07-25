acdd7437064fa67c82b07d5f148cef2d028113a9
NKuzmin <nkuzmin@korusconsulting.ru>
2023-07-25 10:55:51 +0300

    Fix background coloring

--- lua/timetorave/core/colorscheme.lua
+++ lua/timetorave/core/colorscheme.lua
@@ -1,5 +1,5 @@
 vim.g.nord_contrast = false 
-vim.g.nord_disable_background = true 
+vim.g.nord_disable_background = false
 vim.g.nord_italic = false
 vim.g.nord_bold = true 
 vim.g.nord_uniform_diff_background = true
